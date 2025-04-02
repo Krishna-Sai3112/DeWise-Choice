29a5f0a063700b1800e97a2f75df0e51e40a4537
Nagendra Krishna Sai <Krishnasai.achanta31@gmail.com>
2025-04-02 21:12:26 +0530

    Update next.config.mjs

--- dewise-choice/next.config.mjs
+++ dewise-choice/next.config.mjs
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} 
 const nextConfig = {};
 
 export default nextConfig;
+*/
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  assetPrefix: isProd ? '/DeWise-Choice/' : '',
+  images: {
+    unoptimized: true,
+  },
+};
